74d92031df19c5250459dbd9e5c1dd855b6998a9
Andrei Tognolo <andreitognolo@gmail.com>
2016-02-17 12:00:02 -0200

    Getting server port from env

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.listen(3000);
+var port = process.env.PORT || 5000;
+app.listen(port);
 
-
-
-console.log("Running at Port 3000");
+console.log("Running at Port " + port);
